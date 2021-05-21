19055adaeb53e6b880120a6e2668d74558d6cabb
Jodie Putrino <j.putrino@f5.com>
2021-05-21 14:14:06 -0600

    Update Makefile

--- docs-web/conf.py
+++ docs-web/conf.py
@@ -48,7 +48,7 @@ source_suffix = {
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_source']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_source/']
 
 
 # -- Options for HTML output -------------------------------------------------
