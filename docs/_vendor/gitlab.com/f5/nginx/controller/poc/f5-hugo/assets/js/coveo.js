1ab059b9d0b10f884e781fae0f191b63a2a3fa6e
Jodie Putrino <j.putrino@f5.com>
2021-06-25 11:33:30 -0600

    pull latest theme updates from development branch

---
+++ docs/_vendor/gitlab.com/f5/nginx/controller/poc/f5-hugo/assets/js/coveo.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // 2. Configure a search endpoint
+  Coveo.SearchEndpoint.configureCloudV2Endpoint("", 'xxfabb573f-7116-4a17-ac25-b18118218ae0');
+  const root = document.getElementById("search");
+
+  const searchBoxRoot = document.getElementById("searchbox");
+  Coveo.initSearchbox(
+    searchBoxRoot,
+    "/search");
+
+  /*var resetbtn = document.querySelector('#reset_btn');
+  resetbtn.onclick = function () {
+    document.querySelector('.coveo-facet-header-eraser').click();
+  };
+  Coveo.$$(root).on("querySuccess", function(e, args) {
+      resetbtn.style.display="block";
+    });*/
+
+  Coveo.init(root);
+})
+
--- docs/go.mod
+++ docs/go.mod
@@ -2,4 +2,4 @@ module github.com/nginxinc/kubernetes-ingress/docs
 
 go 1.15
 
-require gitlab.com/f5/nginx/controller/poc/f5-hugo v0.8.1-0.20210525193013-702780703489 // indirect
+require gitlab.com/f5/nginx/controller/poc/f5-hugo v0.9.1-0.20210625172823-d8362cced04c // indirect
